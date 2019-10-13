4e48f1e9d2ad414ed652daa0f0f00362674b9364
harshanip <perera.harshani@gmail.com>
2019-10-14 08:19:39 +1300

    Reverse substrings with a defined length alternatively. Add a utility to print two diamentional matrices

--- src/main/java/com/leetcode/Util.java
+++ src/main/java/com/leetcode/Util.java
@@ -1,4 +1,13 @@
 package com.leetcode;
 
 public class Util {
+    public static void printMatrix(int[][] coinValues) {
+        for (int i = 0; i < coinValues.length; i++) {
+            for (int j = 0; j < coinValues.length; j++) {
+                System.out.printf("%5d", coinValues[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
--- src/main/java/com/leetcode/mocktest/ReverseString2.java
+++ src/main/java/com/leetcode/mocktest/ReverseString2.java
@@ -1,4 +1,37 @@
 package com.leetcode.mocktest;
 
+import org.junit.Test;
+import static  org.junit.Assert.*;
 public class ReverseString2 {
+    public String reverseStr(String s, int k) {
+        String result="";
+        boolean reverse=true;
+        for(int i=0;i<s.length();i=i+k){
+            result+=reverse(s,i,i+k-1,reverse);
+            reverse= !reverse;
+        }
+        return result;
+    }
+
+    String reverse(String s, int start, int end,boolean isReverse) {
+        if (s.length() <=end)
+            end=s.length()-1;
+        if(!isReverse){
+            return s.substring(start,end+1);
+        }else {
+            String r="";
+            for(int i=end;i>=start;i--){
+                r+=s.charAt(i);
+            }
+            return r;
+        }
+    }
+@Test
+    public  void testMethod() {
+        String s = "abcdefg";
+       String actual = new ReverseString2().reverseStr(s,2);
+       assertEquals("bacdfeg",actual);
+     actual = new ReverseString2().reverseStr(s,8);
+    assertEquals("gfedcba",actual);
+    }
 }
